8808d0732239b35dd38c6022a85b476e833f673f
Ahmed <ahmed@localhost.localdomain>
2017-10-28 20:35:32 +0200

    finished implementing post/get for carmodels and get for employees

--- carshopserver.js
+++ carshopserver.js
@@ -2,27 +2,23 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
+var fs = require('fs');
 var app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.locals.data = require("./data.json");
-
-var todoItems = [
-	{id: 1, desc: "a"},
-	{id: 2, desc: "b"},
-	{id: 3, desc: "c"}
-];
-
 
 app.get('/', function (req, res) {
 	res.render("index");
 });
 
 app.get('/employees', function (req, res) {
-	res.end(JSON.stringify(app.locals.data.carshop.employees));
+	res.charset = "UTF-8";
+	res.render("employees");
 });
 
 app.get('/carmodels', function (req, res) {
@@ -30,12 +26,16 @@ app.get('/carmodels', function (req, res) {
 });
 
 app.post("/carmodels", function(req, res){
-	var newItem = req.body.newItem;
-	
-	todoItems.push({
-		id: todoItems.length + 1,
-		desc: newItem
+	var brand = req.body.brand;
+	var model = req.body.model;
+	var price = req.body.price;
+	app.locals.data.carshop.carmodels.push({
+		"id": app.locals.data.carshop.carmodels.length + 1,
+		"brand": brand,
+		"model": model,
+		"price": parseInt(price)
 	});
+	fs.writeFile("data.json", JSON.stringify(app.locals.data, null, 2), "utf8");
 	res.redirect("/carmodels");
 });
 
